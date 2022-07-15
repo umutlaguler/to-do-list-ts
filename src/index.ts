fdb7e967ae5eba2d02bf95fbf124c53d8002b47b
perseusss <ed>
2022-07-16 00:09:30 +0300

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,56 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid"
+import { title } from "process"
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
-import confetti from 'canvas-confetti';
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(newTask)
+  saveTasks()
+  addListItem(newTask)
+  input.value = ""
+})
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkBox = document.createElement("input")
+  const deleteBtn = document.createElement("button")
+  deleteBtn.textContent = 'Delete';
+  checkBox.addEventListener("change", () => {
+    task.completed = checkBox.checked
+    saveTasks()
+  })
+  checkBox.type = "checkbox"
+  checkBox.checked = task.completed
+  label.append(checkBox, task.title)
+  label.append(deleteBtn)
+  item.append(label)
+  list?.append(item)
+}
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
